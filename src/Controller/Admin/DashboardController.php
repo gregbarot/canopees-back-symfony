6766b281fbb8e1d3c7c8e59b181251995f1c32ff
Gregory Barot <gregbarot@gmail.com>
2026-07-03 16:01:33 +0200

    test de creation de crud et ajout d'une page d'accueil pour le dashboard + installation des asset easy admin pour un style convenable

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -7,13 +7,21 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
+// use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 
 #[AdminDashboard(routePath: '/admin', routeName: 'admin')]
 class DashboardController extends AbstractDashboardController
 {
     public function index(): Response
     {
-        return parent::index();
+        // return parent::index();
+
+        return $this->render('admin/dashboard.html.twig');
+
+        // $routeBuilder = $this->container->get(AdminUrlGenerator::class);
+        // $url = $routeBuilder->setController(PageContentCrudController::class)->generateUrl();
+        // return $this->redirect($url);
+
 
         // Option 1. You can make your dashboard redirect to some common page of your backend
         //
@@ -34,12 +42,18 @@ class DashboardController extends AbstractDashboardController
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('Canopees Back Symfony');
+            ->setTitle('Canopées - Back-Office');
     }
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToDashboard('Tableau de bord', 'fa fa-home');
+
+        // yield MenuItem::section('Pages');
+        yield MenuItem::linkTo(PageContentCrudController::class, 'Contenus des pages', 'fa fa-file-list');
+
+        // yield MenuItem::section('Bios');
+        yield MenuItem::linkTo(BioCrudController::class, 'Contenus des bios', 'fa fa-file-list');
         // yield MenuItem::linkTo(SomeCrudController::class, 'The Label', 'fas fa-list');
     }
 }
