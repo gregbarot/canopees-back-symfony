756f5abc1a467accdea269e7fdec0a0ba4741f9c
Gregory Barot <gregbarot@gmail.com>
2026-07-06 15:04:13 +0200

    configuration crud service

--- src/Controller/Admin/ServiceCrudController.php
+++ src/Controller/Admin/ServiceCrudController.php
@@ -3,6 +3,9 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Service;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
@@ -15,14 +18,38 @@ class ServiceCrudController extends AbstractCrudController
         return Service::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Prestation')
+            ->setEntityLabelInPlural('Prestations')
+            ->setPageTitle(Crud::PAGE_INDEX, 'Prestations et tarifs')
+            ->setPageTitle(Crud::PAGE_EDIT, 'Modifier une prestation')
+            ->setDefaultSort(['id' => 'ASC']);
+    }
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')
+                ->hideOnForm(),
+
+            TextField::new('name', 'Nom de la prestation'),
+
+            TextEditorField::new('description', 'Description')
+                ->setHelp('Texte de présentation de la prestation.'),
+
+            TextField::new('color', 'Couleur')
+                ->setHelp('Classe ou code couleur utilisé côté front, par exemple : green, brown, #6B8E23...'),
+
+            TextField::new('priceText', 'Tarif')
+                ->setHelp('Exemple : À partir de 45 € / heure'),
         ];
     }
-    */
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->disable(Action::NEW, Action::DELETE);
+    }
 }
