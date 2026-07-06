82e96890a89c8df44786c765dfc947bca1aec58e
Gregory Barot <gregbarot@gmail.com>
2026-07-06 11:19:11 +0200

    Configuration ContactMEssage

--- src/Controller/Admin/ContactMessageCrudController.php
+++ src/Controller/Admin/ContactMessageCrudController.php
@@ -3,9 +3,15 @@
 namespace App\Controller\Admin;
 
 use App\Entity\ContactMessage;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class ContactMessageCrudController extends AbstractCrudController
@@ -15,14 +21,62 @@ class ContactMessageCrudController extends AbstractCrudController
         return ContactMessage::class;
     }
 
-    /*
+
+    //configuration de l'apparence et comportements du CRUD
+        public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Message de contact')
+            ->setEntityLabelInPlural('Messages de contact')
+            ->setPageTitle(Crud::PAGE_INDEX, 'Messages de contact')
+            ->setPageTitle(Crud::PAGE_DETAIL, 'Détail du message')
+            ->setPageTitle(Crud::PAGE_EDIT, 'Traitement du message')
+            ->setDefaultSort(['sentAt' => 'DESC']);
+    }
+    
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+           IdField::new('id')
+                ->hideOnForm(),
+
+            TextField::new('name', 'Nom'),
+
+            EmailField::new('email', 'Email'),
+
+            TextField::new('phone', 'Téléphone'),
+
+            TextField::new('serviceRequested', 'Prestation demandée'),
+
+            TextareaField::new('message', 'Message')
+                ->hideOnIndex(),
+
+            DateTimeField::new('sentAt', 'Date d’envoi')
+                ->hideOnForm(),
+
+            BooleanField::new('isRead', 'Lu'),
+
+            BooleanField::new('isAnswered', 'Répondu'),
         ];
     }
-    */
+
+
+    //actions personnalisées.
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->disable(Action::NEW)
+            ->add(Crud::PAGE_INDEX, Action::DETAIL)
+            ->update(Crud::PAGE_INDEX, Action::DETAIL, function (Action $action) {
+                return $action
+                    ->setLabel('Lire')
+                    ->setIcon('fa fa-eye');
+            })
+            ->update(Crud::PAGE_INDEX, Action::EDIT, function (Action $action) {
+                return $action
+                    ->setLabel('Traiter')
+                    ->setIcon('fa fa-pen');
+            });
+    }
+    
 }
--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -4,8 +4,10 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+// use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class UserCrudController extends AbstractCrudController
@@ -15,14 +17,24 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+             IdField::new('id')
+                ->hideOnForm(),
+
+            EmailField::new('email', 'Email'),
+
+            TextField::new('name', 'Nom'),
+
+            ChoiceField::new('roles', 'Rôles')
+                ->setChoices([
+                    'Administrateur' => 'ROLE_ADMIN',
+                    'Utilisateur' => 'ROLE_USER',
+                ])
+                ->allowMultipleChoices(),
         ];
     }
-    */
+    
 }
