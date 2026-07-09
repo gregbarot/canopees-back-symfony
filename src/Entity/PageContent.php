a9325c2a18aa122f7d3f97ab03fe6ca2e684bf60
Gregory Barot <gregbarot@gmail.com>
2026-07-09 10:46:45 +0200

    Ajoute les groupes de sérialisation pour PageContent

--- src/Entity/PageContent.php
+++ src/Entity/PageContent.php
@@ -3,32 +3,47 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use Symfony\Component\Serializer\Attribute\Groups;
 use App\Repository\PageContentRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: PageContentRepository::class)]
-#[ApiResource()]
+#[ApiResource(
+    operations: [
+        new GetCollection(),
+        new Get(),
+    ],
+    normalizationContext: ['groups' => ['page_content:read']]
+)]
 class PageContent
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['page_content:read'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 100)]
+    #[Groups(['page_content:read'])]
     private ?string $page = null;
 
     #[ORM\Column(length: 100)]
+    #[Groups(['page_content:read'])]
     private ?string $section = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['page_content:read'])]
     private ?string $title = null;
 
     #[ORM\Column(length: 255, nullable: true)]
+    #[Groups(['page_content:read'])]
     private ?string $subtitle = null;
 
     #[ORM\Column(type: Types::TEXT, nullable: true)]
+    #[Groups(['page_content:read'])]
     private ?string $textContent = null;
 
     public function getId(): ?int
